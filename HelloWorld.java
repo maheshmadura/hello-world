b46dedf9179e64d011d197198341ed081c82a3f5
maheshmadura <maheshkumarcs.51@gmail.com>
2021-07-28 18:20:25 +0000

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello world to CTS!");
+    System.out.println("Hello world to developer mode!");
   }
 }
